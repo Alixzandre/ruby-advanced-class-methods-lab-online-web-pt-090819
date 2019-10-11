f085529c6ded6320472497cf71b6d16936a0d0dc
Alexander Bispo <alexbispo173@gmail.com>
2019-10-11 02:52:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,7 +58,7 @@ class Song
   
   def self.create_from_filename(title)
     result = self.new_from_filename(title)
-    self.create
+    song = self.create
     song.name = result.name
     song.artist_name = result.artist_name
     song
